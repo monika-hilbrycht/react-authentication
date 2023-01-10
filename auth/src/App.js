2b5cfd62ecd1c6e7e95f2ee9a9049aa99b6a0173
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-10 15:57:14 +0100

    structure

---
+++ auth/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>User Authentication</h1>
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ auth/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../components/Login.css';
+
+function Login(){
+
+    return <div>
+        <div className='row justify-content-center'>
+            <div className='col-md-4'>
+                <form>
+                    <input type="text" placeholder='username' className='form-control'></input>
+                    <input type="text" placeholder='password' className='form-control'></input>
+                    <input type="submit" className='btn btn-primary'></input>
+                </form>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Login;
