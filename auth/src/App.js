60239d10cc4e0942b708705da85a66a192711867
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-10 16:02:01 +0100

    styling

--- auth/src/App.js
+++ auth/src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-    <h1>User Authentication</h1>
     <Login/>
     </div>
   );
--- auth/src/components/Login.js
+++ auth/src/components/Login.js
@@ -7,6 +7,7 @@ function Login(){
     return <div>
         <div className='row justify-content-center'>
             <div className='col-md-4'>
+                <h1>User Authentication</h1>
                 <form>
                     <input type="text" placeholder='username' className='form-control'></input>
                     <input type="text" placeholder='password' className='form-control'></input>
