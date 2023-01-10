cee557970395f002d6982502f18ed9484d3aa145
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-10 16:08:40 +0100

    authentication

--- auth/src/components/Login.js
+++ auth/src/components/Login.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../components/Login.css';
 
 function Login(){
 
+    const[username,setusername] = useState('')
+    const[password,setpassword] = useState('')
+
+    function validate() {
+
+        if(username=='javascript' && password=='react') {
+            alert('Login Successfull');
+        } else {
+            alert('Login is failed')
+        }
+    }
+
     return <div>
         <div className='row justify-content-center'>
             <div className='col-md-4'>
                 <h1>User Authentication</h1>
-                <form>
-                    <input type="text" placeholder='username' className='form-control'></input>
-                    <input type="text" placeholder='password' className='form-control'></input>
+                <form onSubmit={validate}>
+                    <input type="text" placeholder='username' className='form-control' value={username} onChange={(e)=>{setusername(e.target.value)}}/>
+                    <input type="text" placeholder='password' className='form-control' value={password} onChange={(e)=>{setpassword(e.target.value)}}/>
                     <input type="submit" className='btn btn-primary'></input>
                 </form>
             </div>
